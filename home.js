ef7117114f64b9305df2c3fc938fe460b48930ea
Piyush Gupta <77025176+Piyush-Guptaa@users.noreply.github.com>
2021-11-12 11:06:30 +0530

    design update

---
+++ home.js
@@ -0,0 +1,9 @@
+document.getElementById('myvideo1').addEventListener('ended',myHandler);
+function myHandler(e) {
+window.location="aboutus.html";
+};
+
+document.getElementById('myvideo2').addEventListener('ended',myHandler);
+function myHandler(e) {
+window.location="aboutus.html";
+};
